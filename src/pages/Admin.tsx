4c42efaaf2e64c7e5cc48be39c9d83de2058a423
CheddarChoi <cde1103@kaist.ac.kr>
2021-12-09 11:19:16 +0900

    Add admin features

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -7,6 +7,7 @@ import { Button } from "antd";
 import { labels } from "../variables/label-info";
 // import videoInfo from "../assets/videoInfo.json";
 import videoInfo from "../assets/videoInfo.json";
+import newVideoInfo from "../assets/newvideoinfo.json";
 import musicvideoInfo from "../assets/musicvideoinfo.json";
 import tutorialvideoInfo from "../assets/tutorialvideoinfo.json";
 
@@ -68,10 +69,82 @@ const removeAllvideos = (videoInfo: any[]) => {
   });
 };
 
+const downloadAllSegments = () => {
+  let csvContent = "data:text/csv;charset=utf-8,";
+  console.log(csvContent);
+
+  firebase
+    .firestore()
+    .collection("videos")
+    .get()
+    .then((snap) => {
+      snap.forEach((doc) => {
+        let videoid = doc.id;
+        console.log(videoid);
+        doc.ref
+          .collection("segments")
+          .orderBy("startTime")
+          .get()
+          .then((snap2) => {
+            snap2.forEach((doc) => {
+              let data = doc.data();
+              let rowArray = [
+                videoid,
+                data.startTime,
+                data.endTime,
+                data.label,
+              ];
+              let row = rowArray.join(",");
+              csvContent += row + "\r\n";
+            });
+          });
+      });
+      setTimeout(() => {
+        var encodedUri = encodeURI(csvContent);
+        window.open(encodedUri);
+      }, 10000);
+    });
+};
+
+const downloadAllNotes = () => {
+  let csvContent = "data:text/csv;charset=utf-8,";
+  console.log(csvContent);
+
+  firebase
+    .firestore()
+    .collection("videos")
+    .get()
+    .then((snap) => {
+      snap.forEach((doc) => {
+        let videoid = doc.id;
+        console.log(videoid);
+        doc.ref
+          .collection("note")
+          .orderBy("videoTimestamp")
+          .get()
+          .then((snap2) => {
+            snap2.forEach((doc) => {
+              let data = doc.data();
+              let rowArray = [videoid, data.videoTimestamp, data.content];
+              let row = rowArray.join(",");
+              csvContent += row + "\r\n";
+            });
+          });
+      });
+      setTimeout(() => {
+        var encodedUri = encodeURI(csvContent);
+        window.open(encodedUri);
+      }, 10000);
+    });
+};
+
 const Admin: React.FC<AdminProps> = (props) => {
   return (
     <>
-      <Button onClick={() => addAllVideos(videoInfo)}>
+      <Button onClick={downloadAllSegments}>Download segment data</Button>
+      <Button onClick={downloadAllNotes}>Download note data</Button>
+      <br />
+      <Button onClick={() => addAllVideos(newVideoInfo)}>
         Add Youtube videos to firebase
       </Button>
       <Button onClick={() => addAllVideos(musicvideoInfo)}>
@@ -80,6 +153,7 @@ const Admin: React.FC<AdminProps> = (props) => {
       <Button onClick={() => addAllVideos(tutorialvideoInfo)}>
         Add tutorial videos to firebase
       </Button>
+      <br />
       <Button onClick={() => removeAllvideos(musicvideoInfo)}>
         Remove all MUSIC videos from firebase
       </Button>
